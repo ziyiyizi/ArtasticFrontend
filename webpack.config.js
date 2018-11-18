c34e3dd2431ae82458372d25e3ac31b903331d1a
Aquamarino <myjasson@163.com>
2018-11-18 09:27:06 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+
+    rules: [{
+            test: /(\.jsx|\.js)$/,
+            use: {
+                loader: "babel-loader"
+            },
+            exclude: /node_modules/
+        },
+        {
+                   test: /\.css$/,
+                   use: [
+                     'style-loader',
+                     'css-loader'
+                   ]
+                 },
+                        {
+                           test: /\.(png|svg|jpg|gif)$/,
+                           use: [
+                           'file-loader'
+                           ]
+                         }
+    ]
+ },
+plugins: [
+
+]
+};
