288ebb370f641a84cd92edd3f32e64395f29293c
Aquamarino <myjasson@163.com>
2018-11-20 17:43:38 +0800

    navbar

--- src/BannerMod.js
+++ src/BannerMod.js
@@ -1,27 +1,86 @@
 import React, { Component } from 'react';
 import logo from './Artastic.png';
-import './BannerMod.css';
 //import './BannerMod.css';
+import {Navbar, Nav, NavDropdown, Button, Form, FormControl, ButtonToolbar} from 'react-bootstrap'
+import './bootstrap.min.css';
 
 class BannerMod extends Component {
   render() {
     return (
-      <div className="BannerMod">
+      
+      <Navbar bg="dark" variant="dark" >
+<Navbar.Brand href="#home">
+        <img
+          alt="no img"
+          src={logo}
+          width="60"
+          height="60"
+          className="d-inline-block align-top"
+        />
+              </Navbar.Brand>
 
-        <ul>
-          <img src={logo} className="logo" alt="logo" />
-          <h1>Artastic</h1>
+  <Navbar.Brand href="#home" >Artastic</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
 
-          <div class="banner">
-          <ul><button class="community">Community</button>
-          <button class="home">Home</button>
-          <button class="search">Search</button>
-          <button class="user">User</button>
-          <button class="notification">Notifications</button></ul>
-          </div>
-                    </ul>
+    </Nav>
 
-      </div>
+  </Navbar.Collapse>
+  <Nav >
+      <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+      <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+    </Nav>
+</Navbar>
+
+
+    
+   /** 
+    * 
+    *       
+
+
+
+
+
+   <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+
+  </Navbar.Collapse>
+  <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+</Navbar>
+**/
     );
   }
 }
