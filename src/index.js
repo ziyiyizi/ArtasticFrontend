0e036ae8fd68b262de7019dd57bb2c2490cdb16d
Aquamarino <myjasson@163.com>
2018-11-28 15:50:56 +0800

    30%

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import BannerMod from './BannerMod';
 import CommunityPage from './CommunityPage';
 import PostList from './Postlist';
 import WelcomeMod from './WelcomeMod';
+import Login from './LoginPage';
 
-ReactDOM.render(<BannerMod/>,document.getElementById('welcome'));
+ ReactDOM.render(<BannerMod/>,document.getElementById('welcome'));
 
-ReactDOM.render(<CommunityPage/>,document.getElementById("CommunityPage"));
-ReactDOM.render(<PostList/>,document.getElementById("CommunityContentPanel"))
+//  ReactDOM.render(<CommunityPage/>,document.getElementById("CommunityPage"));
+//  ReactDOM.render(<PostList/>,document.getElementById("CommunityContentPanel"))
+
+//ReactDOM.render(<Login/>,document.getElementById("CommunityPage"));
+
+ReactDOM.render(<WelcomeMod/>,document.getElementById('CommunityPage'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
