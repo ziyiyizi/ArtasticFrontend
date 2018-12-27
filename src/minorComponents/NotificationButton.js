68e29946768ecf2d194bfcfeb928b6206e13ce26
Aquamarino <myjasson@163.com>
2018-12-27 20:25:24 +0800

    全面使用websocket收取通知

--- src/minorComponents/NotificationButton.js
+++ src/minorComponents/NotificationButton.js
@@ -58,7 +58,7 @@ constructor(props){
   
 
   componentDidMount(){
-    this.interval = setInterval(()=>this.ticking(),10000)
+//    this.interval = setInterval(()=>this.ticking(),10000)
 
   // graceful shutdown
 // Reconnect 10s later
@@ -140,30 +140,6 @@ constructor(props){
 
   render() {
     const { classes } = this.props;
-
-    const sideList = (
-      <div className={classes.list}>
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </div>
-    );
-
-  
 
     return (
       <div>
